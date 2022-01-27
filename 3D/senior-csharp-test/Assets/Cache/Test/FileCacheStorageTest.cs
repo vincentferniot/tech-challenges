f82a17b8bbdc165338324f06501fdef7ff5a3411
Cyrille Contamine <cyrillelouis.contamine@iwd.fr>
2022-01-27 15:46:50 +0100

    remove hints

--- 3D/senior-csharp-test/Assets/Cache/Test/FileCacheStorageTest.cs
+++ 3D/senior-csharp-test/Assets/Cache/Test/FileCacheStorageTest.cs
@@ -1,12 +1,10 @@
 using NUnit.Framework;
 
-[ TestFixture ]
+[TestFixture]
 public class FileCacheStorageTest
 {
-	// A sample test function to indicate the usage of the Test tag. 
-	// https://docs.unity3d.com/2018.3/Documentation/Manual/testing-editortestsrunner.html
-	[ Test ]
-	public void SampleTest()
-	{
-	}
+    [Test]
+    public void SampleTest()
+    {
+    }
 }
